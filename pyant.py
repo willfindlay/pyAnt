28897002226470f9b88de8b5a731c7a55c5da0f8
HousedHorse <william@wfindlay.com>
2019-05-17 12:47:17 -0400

    added stats

--- pyant.py
+++ pyant.py
@@ -4,6 +4,6 @@ from sim import Simulation
 
 if __name__ == "__main__":
 
-    sim = Simulation(ants=12, x=100, y=100)
+    sim = Simulation(ants=5, x=100, y=100, num_rivers=20)
 
     sim.run()
--- sim.py
+++ sim.py
@@ -6,7 +6,7 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 class Simulation:
-    def __init__(self, seed=None, ants=5, num_rivers=5, x=100, y=100):
+    def __init__(self, seed=None, ants=5, num_rivers=0, x=100, y=100):
         # seed random number generator
         np.random.seed(seed)
 
@@ -18,6 +18,10 @@ class Simulation:
         self.grid = np.array([[Dirt(x, y) for y in range(self.height)] for x in range(self.width)])
         # maintain a queue of moves to be made
         self.move_queue = np.array([])
+
+        self.statechanged = False
+        self.day = 1
+        self.ticks = 0
 
         self.generate_map(ants, num_rivers)
 
@@ -32,69 +36,61 @@ class Simulation:
         while True:
             antx = np.random.randint(0, self.width)
             anty = np.random.randint(0, self.height)
-            if(isinstance(self.grid[antx][anty], Dirt)):
+            if(self.grid[antx][anty].walkable()):
                 break
-        ant = Queen(x=antx, y=anty)
-        self.grid[antx][anty] = ant
-        # place initial workers
-        for _ in range(ants):
-            while True:
-                antx = np.random.randint(0, self.width)
-                anty = np.random.randint(0, self.height)
-                if(isinstance(self.grid[antx][anty], Dirt)):
-                    break
-            ant = Worker(x=antx, y=anty)
-            self.grid[antx][anty] = ant
+        self.queen = Queen(x=antx, y=anty, num_ants = ants)
+        self.grid[antx][anty] = self.queen
 
-    def generate_river(self, initial_direction=0, direction=0, riverx=0, rivery=0, length=0):
+    def generate_river(self, initial_direction=0, direction=0, riverx=0, rivery=0):
         repeat_count = 0
+        length = 0
         # 1 = up
         # 2 = left
         # 3 = down
         # 4 = right
         while True:
-            if repeat_count > 20:
-                return
+            while True:
+                if repeat_count > 20:
+                    return
+                if direction is 0:
+                    riverx = np.random.randint(0, self.width)
+                    rivery = np.random.randint(0, self.height)
+                elif direction is 1:
+                    rivery += 1
+                elif direction is 2:
+                    riverx += 1
+                elif direction is 3:
+                    rivery -= 1
+                elif direction is 4:
+                    riverx -= 1
+                # check if the tile placement would be invalid
+                if riverx >= self.width or riverx < 0 or rivery >= self.height or rivery < 0:
+                    repeat_count += 1
+                    continue
+                if isinstance(self.grid[riverx][rivery], Water):
+                    repeat_count += 1
+                    continue
+                break
+            self.grid[riverx][rivery] = Water(riverx,rivery)
+            # if it's the first tile, pick any direction randomly
             if direction is 0:
-                riverx = np.random.randint(0, self.width)
-                rivery = np.random.randint(0, self.height)
-            elif direction is 1:
-                rivery += 1
-            elif direction is 2:
-                riverx += 1
-            elif direction is 3:
-                rivery -= 1
-            elif direction is 4:
-                riverx -= 1
-            # check if the tile placement would be invalid
-            if riverx >= self.width or riverx < 0 or rivery >= self.height or rivery < 0:
-                repeat_count += 1
-                continue
-            if isinstance(self.grid[riverx][rivery], Water):
-                repeat_count += 1
-                continue
-            break
-        self.grid[riverx][rivery] = Water(riverx,rivery)
-        # if it's the first tile, pick any direction randomly
-        if direction is 0:
-            direction = np.random.randint(1,5)
-        else:
-            # otherwise we want to heavily favor going straight and never go back
-            direction_decision = np.random.randint(0,1000)
-            if direction_decision < 900:
-                direction = initial_direction
-            elif direction_decision < 950:
-                direction = (initial_direction + 2) % 4 + 1
+                direction = np.random.randint(1,5)
             else:
-                direction = (initial_direction + 4) % 4 + 1
-        # randomly cut river short
-        # this is an inverse probability as a function of map size
-        cutoff = np.random.randint(0, (self.width + self.height) / 2 - length)
-        if cutoff < 1:
-            return
-        if initial_direction is 0:
-            initial_direction = direction
-        self.generate_river(initial_direction, direction, riverx, rivery, length + 1)
+                # otherwise we want to heavily favor going straight and never go back
+                direction_decision = np.random.randint(0,1000)
+                if direction_decision < 800:
+                    direction = initial_direction
+                elif direction_decision < 900:
+                    direction = (initial_direction + 2) % 4 + 1
+                else:
+                    direction = (initial_direction + 4) % 4 + 1
+            # randomly cut river short
+            # this is an inverse probability as a function of map size
+            cutoff = np.random.randint(0, (self.width + self.height) - length)
+            if cutoff < 5:
+                return
+            if initial_direction is 0:
+                initial_direction = direction
 
     def generate_rivers(self, num_rivers):
         for _ in range(num_rivers):
@@ -116,13 +112,33 @@ class Simulation:
 
     def draw(self):
         self.assign_colors()
-        self.img = self.ax.imshow(self.colors)
+        self.img  = self.ax.imshow(self.colors)
+        stats = ""
+        stats += f"Day: {self.day}\n"
+        stats += f"Queen Age: {self.queen.age}\n"
+        stats += f"Queen Food: {self.queen.hunger * 100}%\n"
+        stats += f"Queen Water: {self.queen.thirst * 100}%\n"
+        stats += f"Colony Food: {self.queen.food}\n"
+        stats += f"Colony Water: {self.queen.water}\n"
+        stats += f"Colony Eggs: {self.queen.num_eggs()}\n"
+        stats += f"Colony Larvae: {self.queen.num_larvae()}\n"
+        stats += f"Colony Workers: {self.queen.num_workers()}\n"
+        self.info = self.fig.text(0.05, 0.95, stats, fontsize=14, verticalalignment='top',
+                bbox= dict(boxstyle='round', facecolor='wheat'))
 
     def tick(self, frame_number):
-        for col in self.grid:
-            for square in col:
-                if(isinstance(square, Ant)):
-                    square.on_tick()
+        self.ticks += 1
+        if self.ticks % (24 * 36) == 0:
+            self.day += 1
+            self.queen.age += 1
+            self.statechanged = True
+        for worker in self.queen.workers:
+            worker.on_tick()
+        if self.statechanged:
+            self.fig.clf()
+            self.fig.add_axes(self.ax)
+            self.draw()
+        self.statechanged = False
 
     def run(self):
         plt.title("pyAnt")
@@ -205,8 +221,19 @@ class Worker(Ant):
         pass
 
 class Queen(Ant):
-    def __init__(self, x, y):
+    def __init__(self, x, y, num_ants):
         super().__init__(x, y)
+        self.age = 0
+        self.hunger = 1
+        self.thirst = 1
+        self.food = 0
+        self.water = 0
+        self.larvae = []
+        self.eggs = []
+        self.workers = []
+
+        for _ in range(num_ants):
+            self.workers.append(Worker(x,y))
 
     def color(self):
         return (1, 0, 1, 1)
@@ -217,5 +244,14 @@ class Queen(Ant):
 
     def move(self):
         pass
+
+    def num_workers(self):
+        return len(self.workers)
+
+    def num_eggs(self):
+        return len(self.eggs)
+
+    def num_larvae(self):
+        return len(self.larvae)
 
 # }}}
